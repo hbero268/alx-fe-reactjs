da2bb19e0bc96444d1d4b270e85a537839955e43
hbero268 <hbero268@gmail.com>
2025-10-31 22:32:40 +0200

    my welcome component imprementation

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+function WelcomeMessage(){
+    return(
+        <div>
+            <h1> tag text to “Hello everyone, I am learning React at ALX!”.</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+export default WelcomeMessage;
