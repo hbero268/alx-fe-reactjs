e3ec3ee993cd8dc8acffd0d5e87bfa6a5e3f833d
hbero268 <hbero268@gmail.com>
2025-11-09 14:02:10 +0200

    week 10 refactor

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,16 +1,33 @@
-function Header() {
+import React from 'react';
+
+const Header = () => {
   return (
-    <header
-      style={{
-        backgroundColor: "navy",
-        color: "white",
-        padding: "20px",
-        textAlign: "center",
+    <header 
+      style={{ 
+        backgroundColor: 'navy', 
+        color: 'white', 
+        textAlign: 'center',
+        padding: '20px 0',
+        boxShadow: '0 2px 5px rgba(0,0,0,0.2)',
+        marginBottom: '20px'
       }}
     >
-      <h1>My Favorite Cities</h1>
+      <h1 style={{ 
+        margin: 0, 
+        fontSize: '2.5rem',
+        textShadow: '2px 2px 4px rgba(0,0,0,0.3)'
+      }}>
+        My Favorite Cities
+      </h1>
+      <p style={{ 
+        margin: '10px 0 0 0',
+        fontSize: '1.1rem',
+        opacity: 0.9
+      }}>
+        Discover amazing places around the world
+      </p>
     </header>
   );
-}
+};
 
 export default Header;
