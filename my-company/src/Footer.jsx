d344f2d92366961651336df04e88510e81ad5271
hbero268 <hbero268@gmail.com>
2025-11-09 16:44:06 +0200

    my company project

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#282c34',
+      color: 'white',
+      textAlign: 'center',
+      padding: '10px',
+      marginTop: '20px'
+    }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
