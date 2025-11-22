bef09ff7fd6fdaface84b65cafc18a1a85d2ec5f
hbero268 <hbero268@gmail.com>
2025-11-22 05:50:16 +0200

    my searching profile

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,7 @@
 const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
+import axios from "axios";
+
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
